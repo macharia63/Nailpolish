6d41b5f9e20fedc9d6458112334d77e7b824f58d
macharia63 <julia.muthoni@hotmail.com>
2020-10-01 20:56:51 +0000

    Migrated database

--- store/views.py
+++ store/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+from .models import * 
 def store(request):
-    context = {}
-    return render(request,'store/store.html', context)
+	products = Product.objects.all()
+	context = {'products':products}
+	return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
-    return render(request,'store/cart.html', context)
+	context = {}
+	return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
-    return render(request,'store/checkout.html', context)
+	context = {}
+	return render(request, 'store/checkout.html', context)
