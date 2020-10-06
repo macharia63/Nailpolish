04a085cc7ab006c3be4be3f65c06d04a625d5594
macharia63 <julia.muthoni@hotmail.com>
2020-10-06 06:49:18 +0000

    additional files and changes for Heroku

--- Nailpolish_shop/settings.py
+++ Nailpolish_shop/settings.py
@@ -9,12 +9,15 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+ import sys
  import os
- import dj_database_url
+ sys.path.append(os.path.abspath(""))
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -104,7 +107,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
+ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
@@ -123,26 +126,26 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+ LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+ TIME_ZONE = 'UTC'
 
-USE_I18N = True
+ USE_I18N = True
 
-USE_L10N = True
+ USE_L10N = True
 
-USE_TZ = True
+ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+ STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
+ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_URL = '/images/'
+ MEDIA_URL = '/images/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/')
