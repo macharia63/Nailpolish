be0673af94f40e3d937bbe2d6ab25fe12e380c83
macharia63 <julia.muthoni@hotmail.com>
2020-10-07 17:56:05 +0000

    deployment

--- Nailpolish_shop/settings.py
+++ Nailpolish_shop/settings.py
@@ -79,17 +79,10 @@ WSGI_APPLICATION = 'Nailpolish_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangogirls',
-        'USER': 'name',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
- }
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
